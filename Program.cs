40cb12e293d18b70e17a5236c8f5da24f63b36fa
katr5256 <43572157+katr5256@users.noreply.github.com>
2018-11-26 13:20:37 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingReal_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vælg punkt mellem 1-4");
+            int switchCase = Convert.ToInt32(Console.ReadLine());
+            switch (switchCase)
+            {
+                case 1:
+                    Console.WriteLine("1. Registrér timer");
+                     
+
+                    break;
+
+                case 2:
+                    Console.WriteLine("2. Se afsluttede vagter for denne måned");
+
+                    break;
+            }
+        }
+    }
+}
